1f1fd207f64319b6d4c845f9e1fa46e654363f43
Krigpl <3904252+Krigpl@users.noreply.github.com>
2018-03-06 14:39:28 +0100

    Cleanup GNTDeposit (#28)

--- contracts/ReceivingContract.sol
+++ contracts/ReceivingContract.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.4.19;
 /// Contracts implementing this interface are compatible with
 /// GolemNetworkTokenBatching's transferAndCall method
 contract ReceivingContract {
-    function onTokenReceived(address _from, uint _value, bytes _data);
+    function onTokenReceived(address _from, uint _value, bytes _data) public;
 }
