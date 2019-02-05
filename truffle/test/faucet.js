31d9e13ebcc570537237759b0d369c28eafbae77
Krigpl <3904252+Krigpl@users.noreply.github.com>
2019-02-05 15:59:19 +0100

    Migrate GNTB tests to truffle (#44)

--- truffle/test/faucet.js
+++ truffle/test/faucet.js
@@ -1,38 +1,18 @@
 const truffleAssert = require('truffle-assertions');
 
-const GolemNetworkToken = artifacts.require("GolemNetworkToken");
-const Faucet = artifacts.require("Faucet");
+const setup = require('./setup')
 
 contract("Faucet", async accounts => {
-  let golemfactory;
+  let golemfactory = accounts.pop();
   let gnt;
   let faucet;
-  let decimals;
 
   beforeEach("setup", async () => {
-    golemfactory = accounts.pop();
-    let currentBlock = await web3.eth.getBlockNumber();
-    gnt = await GolemNetworkToken.new(
-      golemfactory,
-      golemfactory,
-      currentBlock + 2,
-      currentBlock + 3,
-    );
-    decimals = await gnt.decimals.call();
-
-    let tcr = await gnt.tokenCreationRate.call();
-    let tcc = await gnt.tokenCreationCap.call();
-    let requiredEth = tcc / tcr;
-    await gnt.create({from: golemfactory, value: requiredEth});
-    await gnt.finalize();
-
-    assert.isFalse(await gnt.funding.call());
-
-    faucet = await Faucet.new(gnt.address);
-    await gnt.transfer(faucet.address, tcc, {from: golemfactory});
+    [gnt, faucet] = await setup.deployGntAndFaucet(golemfactory);
   });
 
   it("create", async () => {
+    let decimals = await gnt.decimals.call();
     let account = accounts[0];
     assert.equal(0, await gnt.balanceOf(account));
     await faucet.create({from: account});
