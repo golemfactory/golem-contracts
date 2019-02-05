6cc3ba6b2d895e9a962364033bf652cc5cb25bfe
Igor Adamski <igor@golem.network>
2019-02-05 12:18:02 +0100

    Test faucet on truffle

--- truffle/contracts/HelloWorld.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.21;
-
-contract HelloWorld {
-    function hi(uint256 a) public pure returns (uint256) {
-        return a + 1;
-    }
-}
-
--- truffle/test/helloworld.js
+++
@@ -1,9 +0,0 @@
-const HelloWorld = artifacts.require("HelloWorld");
-
-contract("HelloWorld", async accounts => {
-  it("basic", async () => {
-    let instance = await HelloWorld.deployed();
-    let result = await instance.hi.call(42);
-    assert.equal(43, result);
-  });
-});
