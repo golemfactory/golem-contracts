7aa4973036aa9ac869f714a294eb0c58f1cc55f8
lukasz-glen <29129196+lukasz-glen@users.noreply.github.com>
2020-07-07 22:05:34 +0200

    Create GoverningPreferencesExample.sol

---
+++ other-contracts/GoverningPreferencesExample.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.6.6;
+
+contract GoverningPreferencesExample {
+    struct PreferenceStruct {
+        address addr;
+        uint256 preference;
+    }
+    mapping(address => uint256) preferenceMapping;
+    PreferenceStruct[] preferences;
+    uint256 globalParameter;
+    
+    function getPreference(address _member) public view returns (uint256) {
+        return preferences[preferenceMapping[_member]-1].preference;
+    }
+    function getGlobalParameter() public view returns (uint256) {
+        return globalParameter;
+    }
+    
+    function setPreference(uint256 _value) public returns (bool) {
+        if (preferenceMapping[msg.sender] != 0) {
+            preferences[preferenceMapping[msg.sender]-1].preference = _value;
+        } else {
+            preferences.push(PreferenceStruct(msg.sender,_value));
+            preferenceMapping[msg.sender] = preferences.length;
+        }
+        return true;
+    }
+    
+    function removePreference() public returns (bool) {
+        if (preferenceMapping[msg.sender] == 0) {
+            return false;
+        }
+        if (preferenceMapping[msg.sender] < preferences.length-1) {
+            preferences[preferenceMapping[msg.sender]] = preferences[preferences.length-1];
+            preferenceMapping[preferences[preferences.length-1].addr] = preferenceMapping[msg.sender];
+        }
+        delete preferenceMapping[msg.sender];
+        preferences.pop();
+    }
+    
+    function setGlobalParameter(uint256 _value) public returns (bool) {
+        if (preferences.length == 0) {
+            return false;
+        }
+        uint256 less = 0;
+        uint256 equal = 0;
+        uint256 greater = 0;
+        for (uint j = 0; j < preferences.length; j++) {
+            if (preferences[j].preference < _value) {
+                less ++;
+            } else if (preferences[j].preference < _value) {
+                greater ++;
+            } else {
+                equal ++;
+            }
+        }
+        if (3*less > preferences.length) {
+            return false;
+        }
+        if (3*greater > preferences.length) {
+            return false;
+        }
+        globalParameter = _value;
+        return true;
+    }
+    
+}
